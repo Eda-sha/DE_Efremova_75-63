109a25f8019885e3a12dcabe484d99a3219323f8
Eda_sha <efremovadasha6@gmail.com>
2025-09-24 16:22:44 +0300

    update

---
+++ data_loader.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import requests
+from io import StringIO
+
+FILE_ID = "1Svje8GeeWe-hp_F-FNtnYZEGHWo1Lp-Y"
+file_url = f"https://drive.google.com/uc?export=download&id={FILE_ID}"
+
+response = requests.get(file_url)
+
+raw_data = pd.read_csv(StringIO(response.text), sep=';', encoding='utf-8')
+
+print("Форма данных:", raw_data.shape)
+print("\nПервые 10 строк:")
+print(raw_data.head(10))
